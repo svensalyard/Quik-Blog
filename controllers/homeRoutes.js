948d18171115de164391c8af9a90a8f663145b9f
svensalyard <svensalyard@gmail.com>
2024-03-11 12:20:28 -0700

    Fixed homeRoutes to properly render page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,17 +1,24 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, post, comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  res.render('homepage')
-});
+    try {
+        const posts = await post.findAll({include: [User]});
+        const allPosts = posts.map((post) => post.get());
+        res.render('allposts', {allPosts, loggedIn: req.session.logged_in});
+        } catch (err) {console.log(err);}
+})
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const posts = await post.findByPk(req.params.id, {include: [User]});
+        const individualpost = posts.map((post) => post.get());
+        res.render('individualpost', {individualpost, loggedIn: req.session.logged_in});
+        } catch (err) {console.log(err);}
+})
 
 router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
   res.render('login');
 });
 
--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,9 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const controlPanelRoutes = require('./controlpanel-routes');
-const allPostsRoutes = require('./allPostsRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/controlpanel', controlPanelRoutes);
-router.use('./allposts', allPostsRoutes);
 
 module.exports = router;
